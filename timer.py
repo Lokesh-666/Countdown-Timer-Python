95cfba10322610750783b4e1e8bdea63fb8c66f6
Lokesh-666 <lkblokesh12@gmail.com>
2024-06-12 16:31:12 +0530

    Added timer.py file

---
+++ timer.py
@@ -0,0 +1,15 @@
+import time
+
+def main():
+    duration = int(input("Timer time in seconds? ")) 
+    for x in range(duration, 0, -1):  
+        seconds = x % 60
+        minutes = (int(x / 60)) % 60 
+        hours = int(x/3600)
+        print(f"{hours:02}:{minutes:02}:{seconds:02}")
+        time.sleep(1)
+    
+    print("Time is up!!")
+
+if __name__ == "__main__":
+    main()
